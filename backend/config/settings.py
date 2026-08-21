1db26763128a1581f2134e36cb6dacf360bbef57
Naman7564 <Naman7564@gmail.com>
2026-08-21 12:25:43 +0530

    Add 129.159.21.153 to ALLOWED_HOSTS

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -7,7 +7,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY", "django-insecure-filmora-movie-platfor
 
 DEBUG = os.environ.get("DEBUG", "True").lower() in ("true", "1", "t")
 
-ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", "localhost,127.0.0.1,backend,0.0.0.0").split(",")
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", "localhost,127.0.0.1,backend,0.0.0.0,129.159.21.153").split(",")
 
 INSTALLED_APPS = [
     "django.contrib.admin",
